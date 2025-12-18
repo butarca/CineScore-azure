820e51561c6641c86e73326d70ded6118ea4e3fc
lukaazman <92147962+lukaazman@users.noreply.github.com>
2025-12-18 21:00:27 +0100

    Style updates
    - removed movie titles and view details button
    - minimalistic banner and poster styles
    - top rated pages having different on hover popups
        etc.

--- obj/Debug/net10.0/CineScore.AssemblyInfo.cs
+++ obj/Debug/net10.0/CineScore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c135a6486bb59758f4c3858ef003016ca1625490")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e248b852068f73cac8a37ebd638a16924ed9f3f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
