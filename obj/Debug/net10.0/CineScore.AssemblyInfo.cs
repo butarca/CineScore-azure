367cc067cf3660c93e47bfda4adbba66f24fd4cb
lukaazman <92147962+lukaazman@users.noreply.github.com>
2025-12-30 22:13:27 +0100

    Nav menu style update

--- obj/Debug/net10.0/CineScore.AssemblyInfo.cs
+++ obj/Debug/net10.0/CineScore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d727e7c7aaf6e0af4d8ae3ba57b46366c61163b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7766e85a6d409fae3c776483978c2970f9a55bb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
