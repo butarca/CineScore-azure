0dbb7d623899ff7acca42a3b8a65160df968f3a6
lukaazman <92147962+lukaazman@users.noreply.github.com>
2025-12-30 15:38:24 +0100

    Search by Name
    - Added basic search logic
    - Updated the User Dashboard Styles

--- obj/Debug/net10.0/CineScore.AssemblyInfo.cs
+++ obj/Debug/net10.0/CineScore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90f9a23bb23300565690ea9ae7ebfac287438c60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d727e7c7aaf6e0af4d8ae3ba57b46366c61163b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
