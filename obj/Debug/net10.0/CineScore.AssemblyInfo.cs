56dbd1c13bfb96ca1de31caab0dc6e5b2161010a
lukaazman <92147962+lukaazman@users.noreply.github.com>
2026-01-02 13:11:36 +0100

    MoviesApi Update

--- obj/Debug/net10.0/CineScore.AssemblyInfo.cs
+++ obj/Debug/net10.0/CineScore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7766e85a6d409fae3c776483978c2970f9a55bb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d8c74909390cc386ef162fd5e2cba425ea6cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineScore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
